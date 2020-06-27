6eabbd8ca0233d433bd41a6340a867584bd31f74
Joachim Loge <joachim.loge@igd.fraunhofer.de>
2020-06-27 09:25:08 +0200

    Changed project structure to take my own implementation from previous exercise

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,22 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
+  "dev": {
+    "username": process.env.UDACITY_DB_USERNAME,
+    "password": process.env.UDACITY_DB_PASSWORD,
+    "database": process.env.UDACITY_DB_DATABASE,
+    "host": process.env.UDACITY_DB_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.UDACITY_AWS_MEDIA_BUCKET,
   },
-};
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.UDACITY_JWT
+  }
+}
